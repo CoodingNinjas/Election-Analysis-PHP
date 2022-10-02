8e4d4338a1d2e197993342238c32b9371ad47372
favourloverboy <favourehio2019@gmail.com>
2022-10-02 13:46:11 +0100

    Outer-MVC-Configuration

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<?php 
+    session_start();
+    include('config/dblink.php');
+    $collect = new DB();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <base href="http://localhost/Election-Analysis-PHP/">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- Favicon -->
+
+        <!-- Custom styles for this template-->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
---
+++ main.php
@@ -0,0 +1,9 @@
+<?php include('includes/header.php');?>
+    
+    <?php include($page); ?>
+
+    <!-- Code Start Here -->
+
+    <!-- Code End Here -->
+    
+<?php include ('includes/footer.php'); ?>
