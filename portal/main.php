4162a865ac60f04d6ae78c6606b77b4a23057d49
favourloverboy <favourehio2019@gmail.com>
2022-10-18 09:37:55 +0100

    controlling page access

--- portal/main.php
+++ portal/main.php
@@ -1,4 +1,23 @@
 <?php include('includes/header.php');?>
+<?php
+    session_start();
+    if(!$_SESSION['u_name']){
+        header('location: login.php');
+    }
+
+    $admin = 'admin';
+    $user = 'user';
+
+    if($_SESSION['role'] == '1'){
+        if($url[0] != $admin){
+            echo "<script>  window.location='/Election-Analysis-PHP/portal/admin/dashboard' </script>";
+        }
+    }elseif($_SESSION['role'] == '0'){
+        if($url[0] != $user){
+            echo "<script>  window.location='/Election-Analysis-PHP/portal/user/dashboard' </script>";
+        }
+    }
+?>
     
     <?php include($page); ?>
 
